aa3146e514860734c7ded246c354d35b24e63368
Srimugunthan Dhandapani <sdhandap@akamai.com>
2016-10-28 16:43:24 +0530

    d anagram files

---
+++ AnagramSolver/src/anagramsolver/AnagramSolver.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anagramsolver;
+
+/**
+ *
+ *
+ * http://javahungry.blogspot.com/2013/08/anagram-solver-jumbled-words-to-find.html
+ * https://gist.github.com/qaproxy/422a1437b2c0314a4ece
+ * 
+ * https://github.com/MichaelBoselowitz/Python-Jumble-Solver/blob/master/Python-Jumble-Solver.py
+ * 
+ * Similar probs:
+ * words with vowels in order:
+ * https://github.com/archer884/vchk
+ * 
+ * Spell checker
+ * https://github.com/hekoru/pyspellcheck
+ * 
+ */
+public class AnagramSolver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
