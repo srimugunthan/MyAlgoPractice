5d8a2bbb6f5cf9ae706c4e564e23ea9651b0ea14
Srimugunthan Dhandapani <sdhandap@akamai.com>
2016-10-31 20:49:21 +0530

    maxsofar

--- SubArraySum/src/subarraysum/SubArraySum.java
+++ SubArraySum/src/subarraysum/SubArraySum.java
@@ -13,53 +13,60 @@ public class SubArraySum {
 
     /**
      * http://www.geeksforgeeks.org/find-subarray-with-given-sum/
+     *
+     * Max subArray
+     * http://cs.slu.edu/~goldwasser/courses/slu/csci314/2012_Fall/lectures/maxsubarray/
      */
-    
-     /* Returns true if the there is a subarray of arr[] with sum equal to
+    int maxSubArray(int arr[]) {
+        int maxsofar = 0;
+        int maxendinghere = 0;
+        int maxhere = 0;
+        for (int i = 0; i < arr.length; i++) {
+            maxhere = Math.max(maxhere + arr[i], 0);
+            maxsofar = Math.max(maxsofar, maxhere);
+        }
+        return maxsofar;
+    }
+
+    /* Returns true if the there is a subarray of arr[] with sum equal to
        'sum' otherwise returns false.  Also, prints the result */
-    int subArraySum(int arr[], int n, int sum) 
-    {
+    int subArraySum(int arr[], int n, int sum) {
         int curr_sum = arr[0], start = 0, i;
- 
+
         // Pick a starting point
-        for (i = 1; i <= n; i++) 
-        {
+        for (i = 1; i <= n; i++) {
             // If curr_sum exceeds the sum, then remove the starting elements
-            while (curr_sum > sum && start < i-1)
-            {
+            while (curr_sum > sum && start < i - 1) {
                 curr_sum = curr_sum - arr[start];
                 start++;
             }
-             
+
             // If curr_sum becomes equal to sum, then return true
-            if (curr_sum == sum) 
-            {
-                int p = i-1;
+            if (curr_sum == sum) {
+                int p = i - 1;
                 System.out.println("Sum found between indexes " + start
                         + " and " + p);
                 return 1;
             }
-             
+
             // Add this element to curr_sum
-            if (i < n)
-            curr_sum = curr_sum + arr[i];
-             
+            if (i < n) {
+                curr_sum = curr_sum + arr[i];
+            }
+
         }
- 
+
         System.out.println("No subarray found");
         return 0;
     }
-    
+
     public static void main(String[] args) {
-        
-        
+
         SubArraySum arraysum = new SubArraySum();
         int arr[] = {15, 2, 4, 8, 9, 5, 10, 23};
         int n = arr.length;
         int sum = 23;
         arraysum.subArraySum(arr, n, sum);
     }
-    
+
 }
-
-
